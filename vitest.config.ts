9b24a80381e4d5f480e45eada558a560f6ecc0eb
Ranieri Althoff <ranisalt@gmail.com>
2023-06-25 03:49:57 +0200

    Simpler path alias for @ in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,5 @@
-import path from "path";
-
 const config = {
-  clearMocks: true,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./"),
-    },
-  },
+  resolve: { alias: { "@": __dirname } },
 };
 
 export default config;
